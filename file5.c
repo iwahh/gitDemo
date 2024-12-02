7618e982b7ebf6285466ff0d525174c53e8f5783
iwahh <2018565021@qq.com>
2024-12-02 18:00:49 +0800

    feat(ci): add new file "file5.c"

---
+++ file5.c
@@ -0,0 +1,37 @@
+$RECYCLE.BIN/
+ADB/
+Android/
+AppGallery/
+CANDriver/
+GD/
+Huawei Share/
+Log/
+OpenVPN/
+Program Files/
+QFIL/
+RK/
+System Volume Information/
+T3/
+Ubuntu23_RK/
+VMware/
+Virtual Machines/
+cpComment/
+data/
+driver/
+edge download/
+gitBash/
+gitStore/
+gitlab找回码/
+modenBin/
+modenBin.zip
+qtCreat/
+share/
+temp/
+tool/
+企业微信/
+固件/
+夸克网盘/
+开机Logo/
+微信/
+钉钉/
+洪神
