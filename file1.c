b31cf5c71756cdc34beb8022cc39a700acc6041e
iwahh <2018565021@qq.com>
2024-12-03 16:47:33 +0800

    feat(ci): 添加对file1.c的修改

--- file1.c
+++ file1.c
@@ -5,3 +5,76 @@ int main()
 	printf("Hello From file1.c \r\n");
 	return 0;
 }
+ref: refs/heads/master
+
+#include <linux/module.h>
+#include <linux/types.h>
+
+
+$RECYCLE.BIN/
+ADB/
+Android/
+AppGallery/
+CANDriver/
+GD/
+Huawei Share/
+Log/
+OpenVPN/
+Program Files/
+QFIL/
+RK/
+System Volume Information/
+T3/
+Ubuntu23_RK/
+VMware/
+Virtual Machines/
+cpComment/
+data/
+driver/
+edge download/
+gitBash/
+gitStore/
+gitlab找回码/
+modenBin/
+modenBin.zip
+qtCreat/
+share/
+temp/
+tool/
+企业微信/
+固件/
+夸克网盘/
+开机Logo/
+微信/
+钉钉/
+洪神
+total 5620
+drwxr-xr-x 1 hr 197609       0 Nov 29 10:00 ./
+drwxr-xr-x 1 hr 197609       0 Nov 29 10:00 ../
+-rw-r--r-- 1 hr 197609   18765 Nov 20  2023 LICENSE.txt
+-rw-r--r-- 1 hr 197609  255199 Nov 20  2023 ReleaseNotes.html
+drwxr-xr-x 1 hr 197609       0 Nov 29 10:00 bin/
+drwxr-xr-x 1 hr 197609       0 Nov 29 10:00 cmd/
+drwxr-xr-x 1 hr 197609       0 Nov 29 10:00 dev/
+drwxr-xr-x 1 hr 197609       0 Nov 29 10:00 etc/
+-rwxr-xr-x 1 hr 197609  137224 Nov 20  2023 git-bash.exe*
+-rwxr-xr-x 1 hr 197609  136696 Nov 20  2023 git-cmd.exe*
+drwxr-xr-x 1 hr 197609       0 Nov 29 10:00 mingw64/
+dr-xr-xr-x 9 hr 197609       0 Dec  2 13:40 proc/
+drwxr-xr-x 1 hr 197609       0 Dec  2 13:38 tmp/
+-rw-r--r-- 1 hr 197609 1375876 Nov 29 10:00 unins000.dat
+-rwxr-xr-x 1 hr 197609 3215208 Nov 29 09:59 unins000.exe*
+-rw-r--r-- 1 hr 197609   24183 Nov 29 10:00 unins000.msg
+drwxr-xr-x 1 hr 197609       0 Nov 29 10:00 usr/
+total 12
+drwxr-xr-x 1 hr 197609  0 Dec  2 11:03 ./
+drwxr-xr-x 1 hr 197609  0 Nov 29 10:01 ../
+drwxr-xr-x 1 hr 197609  0 Dec  2 11:04 .git/
+-rw-r--r-- 1 hr 197609 10 Dec  2 09:11 README.md
+-rw-r--r-- 1 hr 197609 83 Dec  2 11:03 file1.c
+-rw-r--r-- 1 hr 197609 34 Dec  2 13:36 file2.c
+-rw-r--r-- 1 hr 197609 12 Dec  2 09:44 file3.c
+README.md
+file1.c
+file2.c
+file3.c
